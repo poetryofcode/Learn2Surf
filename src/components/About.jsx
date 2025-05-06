e339908fa16836bb6b878be1b9571caed5936e25
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2025-05-05 21:50:08 -0400

    feat: adding about

---
+++ src/components/About.jsx
@@ -0,0 +1,55 @@
+import about from "../assets/about.jpg";
+
+function About() {
+  return (
+    <section className="about">
+      <div className="container">
+        <div className="about__inner">
+          <div className="about__content">
+            <p className="about__subtitle">Who we are</p>
+            <h2 className="about__title">Kailua SURF SCHOOL</h2>
+            <ul className="about-list">
+              <li className="about-item">
+                <h4>Algarve</h4>
+              </li>
+              <li className="about-item">
+                <h4>South coast</h4>
+              </li>
+              <li className="about-item">
+                <h4>Perfect waves for your experience</h4>
+              </li>
+            </ul>
+            <div className="about__text">
+              <p>
+                Was founded in 2006, being the FIRST SURF SCHOOL OF VILAMOURA
+                BEACH, ALGARVE. Our school is certified by the Portuguese
+                Surfing Federation and by Turismo de Portugal. The School aims
+                to teach the art of surfing the waves in a safe, correct and
+                relaxed way, always under the close and attentive supervision of
+                teachers. Thousands of people have already learned from us to
+                slide on the wave wall on top of a board.
+              </p>
+              <p className="about__text">
+                We have facilities that allow our students to equip themselves
+                and keep their belongings safe and at the end of the class they
+                have a warm shower at their disposal. We have a wide variety of
+                safe boards (softboards) and modern wetsuits to teach in a safe
+                and fun way. The heads of the School are trained by the Faculty,
+                experienced and accredited by the Portuguese Surf Federation.
+                The School is open everyday of the year, including weekends and
+                holidays. Hours vary depending on tides and sea conditions.
+              </p>
+            </div>
+          </div>
+          <div className="about__img">
+            <img src={about} alt="image" />
+            <a className="about__link" href="#">
+              activities
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+export default About;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,103 +15,121 @@ export default function Header() {
   //   setResponse(text);
   // }
   return (
-    <header>
-      {/* <button onClick={fetchHandler}>Fetch data</button>
+    <>
+      <header>
+        {/* <button onClick={fetchHandler}>Fetch data</button>
       <p>{response}</p> */}
-      <div className="container">
-        <div className="header__top">
-          <nav className="menu">
-            <ul className="menu__list">
-              <li className="menu__list-item">
-                <a className="menu__list-link" href="#">
-                  home
-                </a>
-              </li>
-              <li className="menu__list-item">
-                <a className="menu__list-link" href="#">
-                  activities
-                </a>
-              </li>
-              <li className="menu__list-item">
-                <a className="menu__list-link" href="#">
-                  pricing plan
-                </a>
-              </li>
-              <li className="menu__list-item">
-                <a className="menu__list-link" href="#">
-                  contact us
-                </a>
-              </li>
-              <li className="menu__list-item">
-                <a className="menu__list-link" href="#">
-                  blog
-                </a>
-              </li>
-              <li className="menu__list-item">
-                <a className="menu__list-link" href="#">
-                  about us
-                </a>
-              </li>
-            </ul>
-          </nav>
-          <a className="phone" href="tel:+14434451191">
-            <img src={phone} alt="phone" />
-            +1(443)445-1191
-          </a>
-        </div>
-        <img className="title-img" src={title} alt="title" />
-        <h1 className="title visually-hidden">Surfing school</h1>
-        <p className="title-text">Kailua, Hawaii</p>
-        <div className="header__content">
-          <div className="header__info">
-            <ul className="header__info-list">
-              <li className="header__info-item">Private lessons</li>
-              <li className="header__info-item">Group lessons</li>
-              <li className="header__info-item">Surf & Sup rentals</li>
-              <li className="header__info-item">Stand up puddle</li>
-              <li className="header__info-item">Surf trips</li>
-            </ul>
-            <div className="header__social">
-              <ul className="header__social-list">
-                <li className="header__social-item">
-                  <a className="header__social-link" href="#">
-                    <img src={facebook} alt="facebook" />
+        <div className="container">
+          <div className="header__top">
+            <nav className="menu">
+              <ul className="menu__list">
+                <li className="menu__list-item">
+                  <a className="menu__list-link" href="#">
+                    home
                   </a>
                 </li>
-                <li className="header__social-item">
-                  <a className="header__social-link" href="#">
-                    <img src={instagram} alt="instagram" />
+                <li className="menu__list-item">
+                  <a className="menu__list-link" href="#">
+                    activities
                   </a>
                 </li>
-                <li className="header__social-item">
-                  <a className="header__social-link" href="#">
-                    <img src={youtube} alt="youtube" />
+                <li className="menu__list-item">
+                  <a className="menu__list-link" href="#">
+                    pricing plan
                   </a>
                 </li>
-                <li className="header__social-item">
-                  <a className="header__social-link" href="#">
-                    <img src={whatsapp} alt="whatsapp" />
+                <li className="menu__list-item">
+                  <a className="menu__list-link" href="#">
+                    contact us
+                  </a>
+                </li>
+                <li className="menu__list-item">
+                  <a className="menu__list-link" href="#">
+                    blog
+                  </a>
+                </li>
+                <li className="menu__list-item">
+                  <a className="menu__list-link" href="#">
+                    about us
                   </a>
                 </li>
               </ul>
-            </div>
+            </nav>
+            <a className="phone" href="tel:+14434451191">
+              <img src={phone} alt="phone" />
+              +1(443)445-1191
+            </a>
           </div>
-          <div className="header__news">
-            <div className="header__news-box">
-              <h3 className="header__news-title">NAZARE. BIG WAvE TRIP</h3>
-              <time className="header__news-subtitle">10/02/2025</time>
-              <a className="header__news-link" href="#">
-                more info
-              </a>
-              <img src={news} alt="news" />
+          <img className="title-img" src={title} alt="title" />
+          <h1 className="title visually-hidden">Surfing school</h1>
+          <p className="title-text">Kailua, Hawaii</p>
+          <div className="header__content">
+            <div className="header__info">
+              <ul className="header__info-list">
+                <li className="header__info-item">
+                  <h4>Private lessons</h4>
+                </li>
+                <li className="header__info-item">
+                  <h4>Group lessons</h4>
+                </li>
+                <li className="header__info-item">
+                  <h4>Surf & Sup rentals</h4>
+                </li>
+                <li className="header__info-item">
+                  <h4>Stand up puddle</h4>
+                </li>
+                <li className="header__info-item">
+                  <h4>Surf trips</h4>
+                </li>
+              </ul>
+              <div className="header__social">
+                <ul className="header__social-list">
+                  <li className="header__social-item">
+                    <a className="header__social-link" href="#">
+                      <img src={facebook} alt="facebook" />
+                    </a>
+                  </li>
+                  <li className="header__social-item">
+                    <a className="header__social-link" href="#">
+                      <img src={instagram} alt="instagram" />
+                    </a>
+                  </li>
+                  <li className="header__social-item">
+                    <a className="header__social-link" href="#">
+                      <img src={youtube} alt="youtube" />
+                    </a>
+                  </li>
+                  <li className="header__social-item">
+                    <a className="header__social-link" href="#">
+                      <img src={whatsapp} alt="whatsapp" />
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </div>
-            <div className="header__buttons">
-              <a className="header__buttons-book">book now</a>
-              <a className="header__buttons-activities">activities</a>
-            </div>{" "}
+            <div className="header__news">
+              <div className="header__news-box">
+                <h3 className="header__news-title">NAZARE. BIG WAvE TRIP</h3>
+                <time className="header__news-subtitle">10/02/2025</time>
+                <a className="header__news-link" href="#">
+                  more info
+                </a>
+                <img src={news} alt="news" />
+              </div>
+              <div className="header__buttons">
+                <a className="header__buttons-book">book now</a>
+                <a className="header__buttons-activities">activities</a>
+              </div>{" "}
+            </div>
           </div>
         </div>
+      </header>
+      <div className="container">
+        <p className="text">
+          "WHAT LOOKS IMPOSSIBLE TODAY, TOMORROW WILL BE YOUR SOURCE OF
+          INSPIRATION AND RELAXATION"
+        </p>
       </div>
-    </header>
+    </>
   );
 }
