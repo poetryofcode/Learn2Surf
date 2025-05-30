47a4470bdb083a355e238404dcaf58b86ed9c71b
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2025-05-30 09:05:29 -0400

    feat: activities

---
+++ src/components/Activities.jsx
@@ -0,0 +1,45 @@
+export default function Activities() {
+  return (
+    <section className="activities">
+      <div className="container">
+        <h3 className="activities__subtitle">activities</h3>
+        <div className="activities__info">
+          <p className="activities__title">
+            without rip currents - SANDY BEACHES your best adventure
+          </p>
+          <div className="activities__text">
+            <p>
+              Welcome to our school! If you've come this far, it's because
+              you're interested in taking your first steps in surfing. It's
+              never too late to start riding the waves, you'll love this new
+              lifestyle! Surfing improves your physical and mental health in a
+              relaxed and fun environment. Vilamoura Surf School also offers
+              group and private lessons in Surf, Stand Up Paddle Board,
+              Longboard, Bodyboard, Skimming and Carver Skate Board. If you are
+              looking for to rent our equipment, we have high performance boards
+              and wetsuits.
+            </p>
+            <a className="header__buttons-book">book now</a>
+          </div>
+        </div>
+        <ul className="activities__list">
+          <li className="activities__item">
+            <h5>Private lessons</h5>
+          </li>
+          <li className="activities__item">
+            <h5>Group lessons</h5>
+          </li>
+          <li className="activities__item">
+            <h5>Surf rentals</h5>
+          </li>
+          <li className="activities__item">
+            <h5>Stand up puddle</h5>
+          </li>
+          <li className="activities__item">
+            <h5>Surf trips</h5>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+}
