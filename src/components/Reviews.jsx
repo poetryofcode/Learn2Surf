d88c6ef959f68f7436e8bd26e8304fec7041b903
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2025-08-17 21:34:45 -0400

    feat: reviews styling

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -2,55 +2,56 @@ import rev1 from "../assets/rev-1.jpg";
 import rev2 from "../assets/rev-2.jpg";
 import rev3 from "../assets/rev-3.jpg";
 import rev4 from "../assets/rev-4.jpg";
+import styles from "./Reviews.module.scss";
 export default function Reviews() {
   return (
-    <section className="reviews">
+    <section className={styles.reviews}>
       <div className="container">
-        <div className="reviews__top">
-          <p className="reviews__subtitle">Reviews</p>
-          <h2 className="reviews__title">let's go surf & sup</h2>
-          <div className="reviews__items">
-            <div className="reviews__item">
-              <img className="reviews__img" src={rev1} alt="" />
-              <blockquote className="blockquote">
-                <p className="blockquote__text">
-                  "I rented the whole equipment for 7 days. New GEAR! 10 stars."
-                </p>
-                <cite className="blockquote__cite">/Ana</cite>
-                <time className="blockquote__time">July 2025</time>
-              </blockquote>
-            </div>
-            <div className="reviews__item">
-              <img className="reviews__img" src={rev2} alt="" />
-              <blockquote className="blockquote">
-                <p className="blockquote__text">
-                  "It was an unforgettable experience. The super professional
-                  instructors. I loved it!"
-                </p>
-                <cite className="blockquote__cite">/Sara</cite>
-                <time className="blockquote__time">October 2025</time>
-              </blockquote>
-            </div>
-            <div className="reviews__item">
-              <img className="reviews__img" src={rev3} alt="" />
-              <blockquote className="blockquote">
-                <p className="blockquote__text">
-                  "I rented the whole equipment for 7 days. New GEAR! 10 stars."
-                </p>
-                <cite className="blockquote__cite">/Ana</cite>
-                <time className="blockquote__time">July 2025</time>
-              </blockquote>
-            </div>
-            <div className="reviews__item">
-              <img className="reviews__img" src={rev4} alt="" />
-              <blockquote className="blockquote">
-                <p className="blockquote__text">
-                  The best experiens in my life
-                </p>
-                <cite className="blockquote__cite">/Tom</cite>
-                <time className="blockquote__time">March 2026</time>
-              </blockquote>
-            </div>
+        <div className={styles.reviews__top}>
+          <p className={styles.reviews__subtitle}>Reviews</p>
+          <h2 className={styles.reviews__title}>let's go surf & sup</h2>
+        </div>
+        <div className={styles.reviews__items}>
+          <div className={styles.reviews__item}>
+            <img className={styles.reviews__img} src={rev1} alt="" />
+            <blockquote className={styles.blockquote}>
+              <p className={styles.blockquote__text}>
+                "I rented the whole equipment for 7 days. New GEAR! 10 stars."
+              </p>
+              <cite className={styles.blockquote__cite}>/Ana</cite>
+              <time className={styles.blockquote__time}>July 2025</time>
+            </blockquote>
+          </div>
+          <div className={styles.reviews__item}>
+            <img className={styles.reviews__img} src={rev2} alt="" />
+            <blockquote className={styles.blockquote}>
+              <p className={styles.blockquote__text}>
+                "It was an unforgettable experience. The super professional
+                instructors. I loved it!"
+              </p>
+              <cite className={styles.blockquote__cite}>/Sara</cite>
+              <time className={styles.blockquote__time}>October 2025</time>
+            </blockquote>
+          </div>
+          <div className={styles.reviews__item}>
+            <img className={styles.reviews__img} src={rev3} alt="" />
+            <blockquote className={styles.blockquote}>
+              <p className={styles.blockquote__text}>
+                "I rented the whole equipment for 7 days. New GEAR! 10 stars."
+              </p>
+              <cite className={styles.blockquote__cite}>/Ana</cite>
+              <time className={styles.blockquote__time}>July 2025</time>
+            </blockquote>
+          </div>
+          <div className={styles.reviews__item}>
+            <img className={styles.reviews__img} src={rev4} alt="" />
+            <blockquote className={styles.blockquote}>
+              <p className={styles.blockquote__text}>
+                The best experiens in my life
+              </p>
+              <cite className={styles.blockquote__cite}>/Tom</cite>
+              <time className={styles.blockquote__time}>March 2026</time>
+            </blockquote>
           </div>
         </div>
       </div>
